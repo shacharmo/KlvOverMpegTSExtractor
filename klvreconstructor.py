cb26a97de47642120e86bb307c7665b94a09478b
Shachar Mossek <shachar.mossek@gmail.com>
2019-12-14 19:36:06 +0200

    Example of analyzing and printing each KLV packet in MPEG-TS in addition to entire buffer

--- klvreconstructor.py
+++ klvreconstructor.py
@@ -1,4 +1,5 @@
 import binascii
+import klvdata
 
 universal_key = binascii.unhexlify('060e2b34020b0101')
 
@@ -11,15 +12,25 @@ def reconstruct_klv_packets(packets):
     """
     is_found = False
     buffer = b''
+    i_buffer = b''
     pts_per_packet = []
 
     for packet in packets:
         if packet["payload_unit_start_indicator"]:
+            if is_found and len(i_buffer):
+                for x in klvdata.StreamParser(i_buffer):
+                    print('intm start')
+                    x.structure()
+                    x.validate()
+                    print('intm end')
+                i_buffer = ''
             is_found = universal_key in packet['payload']
             if is_found:
                 pts_per_packet.append(packet['pes']['pts'] if ('pes' in packet and 'pts' in packet['pes']) else None)
                 key_index = packet['payload'].index(universal_key)
                 buffer += packet['payload'][key_index:]
+                i_buffer = packet['payload'][key_index:]
         elif is_found:
             buffer += packet['payload']
+            i_buffer += packet['payload']
     return buffer, pts_per_packet
